d38ae3ffc867dff3c9ed1cdeda1a714e4445a40d
jerald macachor <jerald.macachor@gmail.com>
2021-03-20 15:32:29 -0700

    color and shadow styling

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
-// todosDisplay
-const todosDisplay = document.getElementById('todo_list');
-const input_text = "todo_input";
-
+// escape malicious html code
 function escapeHtml(unsafe) {
   return unsafe
   .replace(/&/g, "&amp;")
@@ -15,33 +12,48 @@ function escapeHtml(unsafe) {
 function addTodo(event) {
   event.preventDefault();
   
+  // get todo text
   const todo_input = event.target[input_text];
-  const todo = todo_input.value;
-  if (todo === "") return;
+  const todo_text = todo_input.value;
+  if (todo_text === "") return;
 
-  const li = document.createElement('li');
-  const text = document.createTextNode(`${escapeHtml(todo)} `);
-  li.appendChild(text);
+  // create todo element
+  const todo_div = document.createElement('div');
+  todo_div.className = 'todo_div';
+  const todo_text_span = document.createElement('span');
+  todo_text_span.className = 'todo-item';
+  const todo_text_node = document.createTextNode(`${escapeHtml(todo_text)} `);
+  todo_text_span.appendChild(todo_text_node);
+  todo_div.appendChild(todo_text_span);
 
+  // add remove button
   const removeBtn = document.createElement('span');
   const x = document.createTextNode('❌');
   removeBtn.appendChild(x);
-  removeBtn.className = "removeX";
+  removeBtn.className = "remove-x";
   removeBtn.addEventListener('click', removeTodo);
-  li.appendChild(removeBtn);
+  todo_div.appendChild(removeBtn);
   
-
-  todosDisplay.appendChild(li);
+  // display
+  todosDisplay.appendChild(todo_div);
   todo_input.value = "";
 }
-
-const todosForm = document.getElementById('todo_form');
-todosForm.addEventListener('submit', addTodo);
 
 // deletes a todo
 function removeTodo(event) {
   const todo = event.target.parentElement;
-  console.log(todo);
-  todo.remove();
+  todo.classList.add('toRemove');
+  setTimeout(function (){
+    todo.remove();
+  }, 500);
   
 }
+
+const todosDisplay = document.getElementById('todo_list');
+const input_text = "todo_input";
+
+// todosDisplay
+document.addEventListener('DOMContentLoaded', function(event) {
+  const todosForm = document.getElementById('todo_form');
+  todosForm.addEventListener('submit', addTodo);
+})
